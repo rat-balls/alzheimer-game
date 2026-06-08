eb92ce8a45eead55efb0e56330c3f90754805403
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2026-06-08 14:29:32 +0200

    Upgraded arm

--- Scripts/Player/arm_ik_handler.gd
+++ Scripts/Player/arm_ik_handler.gd
@@ -4,18 +4,22 @@ extends Node3D
 @export var wrist_look_at: Node3D
 @export var armature: Skeleton3D
 
+var ik_tween: Tween
+var default_tween: Tween
+
 var arm_bone_idx: int
 var forearm_bone_idx: int
 
 var interact_handler: Node3D
-var grinder_hand_dest: Marker3D
 var default_pos_ik: Vector3
 var default_pos_lookat: Vector3
+
+var holding: bool = false
+var animating: bool = false
 
 func _ready() -> void:
 	var scene_tree = get_tree()
 	interact_handler = scene_tree.get_first_node_in_group("InteractHandler")
-	grinder_hand_dest = scene_tree.get_first_node_in_group("GrinderHandDest")
 	default_pos_ik = wrist_ik.position
 	default_pos_lookat = wrist_look_at.position
 	
@@ -25,20 +29,44 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if(interact_handler.held_object):
 		handle_held_obj(interact_handler.held_object)
-		
 	elif(interact_handler.grabbed_grinder):
 		handle_held_obj(interact_handler.grabbed_grinder)
 	else:
-		wrist_look_at.position = default_pos_lookat
-		wrist_ik.position = default_pos_ik
-		armature.set_bone_pose_scale(arm_bone_idx, Vector3(1, 1, 1))
-		armature.set_bone_pose_scale(forearm_bone_idx, Vector3(1, 1, 1))
+		if(holding):
+			reset_hands()
+
+
+func reset_hands():
+	holding = false
+	if(default_tween): default_tween.kill()
+	
+	default_tween = create_tween()
+	default_tween.set_parallel(true)
+	default_tween.tween_property(wrist_look_at, "position", default_pos_lookat, 0.5).set_trans(Tween.TRANS_QUAD)
+	default_tween.tween_property(wrist_ik, "position", default_pos_ik, 0.5).set_trans(Tween.TRANS_QUAD)
+	armature.set_bone_pose_scale(arm_bone_idx, Vector3(1, 1, 1))
+	armature.set_bone_pose_scale(forearm_bone_idx, Vector3(1, 1, 1))
 
 func handle_held_obj(obj) ->void:
+	
 	var obj_gp = obj.find_child("GrabPointMark")
 	var obj_la = obj.find_child("LookAtMark")
 	if(obj_gp):
-		wrist_look_at.global_position = obj_la.global_position
-		wrist_ik.global_position = obj_gp.global_position
-		armature.set_bone_pose_scale(arm_bone_idx, Vector3(1, armature.global_position.distance_to(obj_gp.global_position) * 0.47, 1))
-		armature.set_bone_pose_scale(forearm_bone_idx, Vector3(1, armature.global_position.distance_to(obj_gp.global_position) * 0.47, 1))
+		if(!holding and !animating): 
+			animating = true
+			holding = true
+			if(ik_tween): ik_tween.kill()
+			
+			ik_tween = create_tween()
+			ik_tween.set_parallel(true)
+			ik_tween.tween_property(wrist_look_at, "global_position", obj_la.global_position, 0.5).set_trans(Tween.TRANS_QUAD)
+			ik_tween.tween_property(wrist_ik, "global_position", obj_gp.global_position, 0.5).set_trans(Tween.TRANS_QUAD)
+			
+			#ik_tween.tween_method({() => animating = false})
+			
+		else:
+			print("here")
+			wrist_look_at.global_position = obj_la.global_position
+			wrist_ik.global_position = obj_gp.global_position
+			armature.set_bone_pose_scale(arm_bone_idx, Vector3(1, clamp(armature.global_position.distance_to(obj_gp.global_position) * 0.47, 0.6, 1.), 1))
+			armature.set_bone_pose_scale(forearm_bone_idx, Vector3(1, clamp(armature.global_position.distance_to(obj_gp.global_position) * 0.47, 0.6, 1), 1))
