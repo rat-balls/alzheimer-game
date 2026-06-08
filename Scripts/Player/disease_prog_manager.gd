7c0bafcc2e7dd32d7933d81331756ebb9edb021d
Paul Rivallin <99149335+Roronoatii@users.noreply.github.com>
2026-06-08 19:56:56 +0200

    Updated disease progress script

--- Scripts/Player/disease_prog_manager.gd
+++ Scripts/Player/disease_prog_manager.gd
@@ -1,11 +1,7 @@
 extends Node
 
+var disease_progress: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("fart")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	print("Disease Stage : ", disease_progress)
