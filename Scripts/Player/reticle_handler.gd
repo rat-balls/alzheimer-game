33900752d08c6f5d7cf24caa7ab8a54c37a77d05
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2026-06-18 21:15:09 +0200

    Added new hand icons

--- Scripts/Player/reticle_handler.gd
+++ Scripts/Player/reticle_handler.gd
@@ -6,6 +6,8 @@ const HOLD_HAND_ICON = preload("uid://qhns457xooh2")
 const INTERACT_HAND_ICON = preload("uid://b2y7mpbrcmiqi")
 const OPEN_HAND_ICON = preload("uid://okh1ikorqos7")
 const RETICLE_ICON = preload("uid://cemueeiiwuruf")
+const GRAB_HAND = preload("uid://bknu03oynqw6a")
+const HOLD_HAND = preload("uid://w6dhxh35rgdn")
 
 func _ready() -> void:
 	interact_handler = get_tree().get_first_node_in_group("InteractHandler")
@@ -13,10 +15,10 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if(interact_handler):
 		if(interact_handler.held_object != null):
-			texture = HOLD_HAND_ICON
+			texture = HOLD_HAND
 		elif(interact_handler.valid_interact_target != null):
 			texture = INTERACT_HAND_ICON
 		elif(interact_handler.valid_hold_target != null):
-			texture = OPEN_HAND_ICON
+			texture = GRAB_HAND
 		else:
 			texture = RETICLE_ICON
