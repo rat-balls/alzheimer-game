cd7eb85c47e2c3f31ed3357afb58fa3c5b3c1607
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2026-06-18 22:38:47 +0200

    Finalized reticles

--- Scripts/Player/reticle_handler.gd
+++ Scripts/Player/reticle_handler.gd
@@ -2,12 +2,10 @@ extends TextureRect
 
 var interact_handler: Node
 
-const HOLD_HAND_ICON = preload("uid://qhns457xooh2")
-const INTERACT_HAND_ICON = preload("uid://b2y7mpbrcmiqi")
-const OPEN_HAND_ICON = preload("uid://okh1ikorqos7")
 const RETICLE_ICON = preload("uid://cemueeiiwuruf")
-const GRAB_HAND = preload("uid://bknu03oynqw6a")
-const HOLD_HAND = preload("uid://w6dhxh35rgdn")
+const FINAL_GRAB = preload("uid://bf81dph7yeeoq")
+const FINAL_INTERACT = preload("uid://djrunetac7dsi")
+const FINAL_OPEN = preload("uid://dpd6e3agpwcsb")
 
 func _ready() -> void:
 	interact_handler = get_tree().get_first_node_in_group("InteractHandler")
@@ -15,10 +13,23 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if(interact_handler):
 		if(interact_handler.held_object != null):
-			texture = HOLD_HAND
+			texture = FINAL_GRAB
 		elif(interact_handler.valid_interact_target != null):
-			texture = INTERACT_HAND_ICON
+			texture = FINAL_INTERACT
 		elif(interact_handler.valid_hold_target != null):
-			texture = GRAB_HAND
+			texture = FINAL_OPEN
 		else:
 			texture = RETICLE_ICON
+		
+		if texture == RETICLE_ICON:
+			print("reticle_icon")
+			rotation_degrees = 0.0
+			size = Vector2(64, 64)
+			position = Vector2(608, 328)
+			pivot_offset = Vector2(32, 32)
+		else:
+			print("hand_icon")
+			rotation_degrees = 135
+			size = Vector2(128, 256)
+			position = Vector2(576, 256)
+			pivot_offset = Vector2(64, 128)
